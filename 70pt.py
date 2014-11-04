d312d25aebd0855d203d793b29eaf4576bf46b94
nickhernandez420 <s571282@student.roundrockisd.org>
2014-11-04 13:32:58 -0600

    70 point complete
    finished 70 point

--- 70pt.py
+++ 70pt.py
@@ -27,8 +27,10 @@ class MyApp:
 		self.button3 = Button(self.myContainer1)
 		self.button3.configure(text="Test?", background="cyan")  
 		self.button3.grid(row=0,column=2)	  ### (3)
-			
-	
+		
+		self.button4 = Button(self.myContainer1)
+		self.button4.configure(text="numbuh" , background="purple")
+		self.button4.grid(row=0,column=2)
 		
 root = Tk()
 myapp = MyApp(root)
